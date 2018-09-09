8412d31fa011cea610ac5bb5d32e5a23cce80867
Thalida Noel <thalida.c.noel@gmail.com>
2018-09-09 13:17:21 -0400

    Updated to v2 route setup

--- app/src/components/Base.vue
+++ app/src/components/Base.vue
@@ -1,20 +1,65 @@
 <template>
   <div id="app">
     <Header />
+    <router-link
+      v-for="(participant, index) in participantOrder"
+      :key="index"
+      :to="{ name: 'Thread', params: {
+        participant: participant,
+        session: selectedSession + 1
+      }}">
+      {{ participant }}
+      <!-- {{ people[participant].full_name }} -->
+    </router-link>
     <router-view />
   </div>
 </template>
 
 <script>
 import Header from './Header';
+// import Thread from './Thread';
 
 export default {
   name: 'Base',
   components: {
     Header,
   },
+  data() {
+    return {
+      participantOrder: ['akilah', 'robyn', 'timothy'],
+      defaultParticipant: 'akilah',
+      defaultSession: 1,
+      people: {},
+    };
+  },
   created() {
     this.$session.start();
+  },
+  mounted() {
+    this.$root.$on('session-select', this.onSessionSelect);
+  },
+  computed: {
+    selectedParticipant() {
+      return this.$route.params.participant || this.defaultParticipant;
+    },
+    selectedSession() {
+      const session = this.$route.params.session || this.defaultSession;
+      return session - 1;
+    },
+    participantThemeClass() {
+      return `theme--${this.selectedParticipant}`;
+    },
+  },
+  methods: {
+    onSessionSelect(data) {
+      this.$router.push({
+        name: 'Thread',
+        params: {
+          participant: data.participant,
+          session: data.session + 1,
+        },
+      });
+    },
   },
 };
 </script>
