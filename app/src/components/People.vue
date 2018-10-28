a6e9bdd29fd1537ff26812e548fe3995f6e53d29
Thalida Noel <thalida.c.noel@gmail.com>
2018-10-27 20:22:01 -0400

    Forgot to commit this and too lazy to redo

---
+++ app/src/components/People.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="people container-wrapper">
+      <span
+        v-for="(participant, index) in participantOrder"
+        class="person"
+        :class="['person--' + participant]"
+        :key="index">
+        <router-link
+          class="person_link"
+          :to="{
+            name: 'Thread',
+            params: {
+              participant: participant,
+              session: selectedSession,
+            }
+          }"
+        >
+          {{people[participant].full_name}}
+        </router-link>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'People',
+  computed: {
+    people() {
+      return this.$store.state.people;
+    },
+    participantOrder() {
+      return this.$store.state.participantOrder;
+    },
+    selectedSession() {
+      return this.$store.state.selectedSession;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/toolkit';
+
+.people {
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  z-index: 1;
+}
+
+.person {
+  margin: 16px 0;
+  text-align: center;
+  font-weight: bold;
+  font-size: 22px;
+  color: $color-dark-gray;
+
+  &_link {
+    display: block;
+    text-decoration: none;
+    transition: color 400ms ease;
+  }
+
+  &:hover {
+    animation-duration: 500ms;
+    animation-name: bounce;
+    animation-fill-mode: backwards;
+  }
+
+  @media (min-width: 500px) {
+    font-size: 32px;
+  }
+
+  @media (min-width: 800px) {
+    font-size: 38px;
+    &:first-child {
+      text-align: left;
+    }
+
+    &:last-child {
+      text-align: right;
+    }
+  }
+}
+</style>
